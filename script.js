0123ab08180f00e5415e8bd265c9945909e0b377
Namelesd <rodalvaram@gmail.com>
2022-11-20 14:40:38 -0600

    Terminando el checkComplete

--- script.js
+++ script.js
@@ -39,8 +39,17 @@ btn.addEventListener("click", createTask)
 const checkComplete= () =>{
     const i= document.createElement("i");
     i.classList.add("far","fa-check-square","icon");
-
+    i.addEventListener("click",completeTask)
     return i;
 
 
 }
+
+ const completeTask = (event)=>{
+const element = event.target ;
+element.classList.toggle("far");
+element.classList.toggle("completeIcon");
+element.classList.toggle("fas");
+
+
+ }
