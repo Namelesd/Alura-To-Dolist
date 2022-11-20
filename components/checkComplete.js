8b7b23aad0515098a713f30b8639f2e50fad4af7
Namelesd <rodalvaram@gmail.com>
2022-11-20 15:14:46 -0600

    Organizando mi proyecto

---
+++ components/checkComplete.js
@@ -0,0 +1,19 @@
+const checkComplete= () =>{
+    const i= document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click",completeTask)
+    return i;
+
+
+}
+
+ const completeTask = (event)=>{
+const element = event.target ;
+element.classList.toggle("far");
+element.classList.toggle("completeIcon");
+element.classList.toggle("fas");
+
+
+ };
+
+ export default checkComplete;
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-( () => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
+
 
 const btn = document.querySelector("[data-form-btn]");
 
@@ -30,40 +33,7 @@ const createTask = (evento)=> {
 
 btn.addEventListener("click", createTask)
 
-const checkComplete= () =>{
-    const i= document.createElement("i");
-    i.classList.add("far","fa-check-square","icon");
-    i.addEventListener("click",completeTask)
-    return i;
 
 
-}
-
- const completeTask = (event)=>{
-const element = event.target ;
-element.classList.toggle("far");
-element.classList.toggle("completeIcon");
-element.classList.toggle("fas");
 
 
- };
-
-const deleteIcon= ()=>{
-    const i= document.createElement("i");
-    i.classList.add("fas","fa-trash-alt","trashIcon","icon");
-    i.addEventListener("click",deleteTask);
-
-
-    return i;
-
-}
-
-const deleteTask = (event) =>{
-
-console.log("Eliminar tarea");
-const parent= event.target.parentElement;
-parent.remove();
-}
-
-
-})();
